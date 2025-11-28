a41246da0ff674b8dd171a0ed4f6b73c648f2ad3
springtiming <yinghuaf@student.unimelb.edu.au>
2025-11-28 17:57:50 +1100

    style: 改用Tailwind 的渐变加载动画

--- src/pages/AdminMembers.tsx
+++ src/pages/AdminMembers.tsx
@@ -438,7 +438,7 @@ export function AdminMembers() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50"
+              className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-[60]"
               onClick={() => setConfirmDialog(null)}
             >
               <motion.div
